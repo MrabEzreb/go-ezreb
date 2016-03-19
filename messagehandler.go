d86f59fce91e9262890a3ada0eece3bcfa4d3e79
Mrab Ezreb <mrabezreb@outlook.com>
2016-03-19 10:37:16 -0400

    fixedstuff

--- messagehandler.go
+++ messagehandler.go
@@ -2,6 +2,8 @@ package ezreb
 
 import (
 	"github.com/nlopes/slack"
+	"strings"
+	"fmt"
 )
 
 func handleMessage(e slack.MessageEvent, rtm *slack.RTM) {
@@ -56,4 +58,120 @@ func handleMessage(e slack.MessageEvent, rtm *slack.RTM) {
 		//		fmt.Printf("Sent to channel %s at %s", channelID, timestamp)
 		rtm.PostMessage(channel.ID, "Backpack.tf Data "+t.Format(time.UnixDate), params)
 	}
+}
+
+type webapiJSON struct {
+	status     string
+	statuscode int
+	body       string
+}
+
+func getWebJSON(url string) (webapiJSON, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+		return *new(webapiJSON), err
+	}
+	robots, err := ioutil.ReadAll(res.Body)
+	//fmt.Printf("%+v\n\n\n\n\n", res)
+	apiJSON := webapiJSON{res.Status, res.StatusCode, string(robots[:])}
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+		return *new(webapiJSON), err
+	}
+	//fmt.Printf("%s", robots)
+	return apiJSON, nil
+}
+
+type Currency struct {
+	Response struct {
+		Currencies struct {
+			Earbuds struct {
+				Blanket   int    `json:"blanket"`
+				Craftable string `json:"craftable"`
+				Defindex  int    `json:"defindex"`
+				Plural    string `json:"plural"`
+				Price     struct {
+					Currency   string  `json:"currency"`
+					Difference float64 `json:"difference"`
+					Value      float64 `json:"value"`
+					ValueHigh  float64 `json:"value_high"`
+				} `json:"price"`
+				Priceindex int    `json:"priceindex"`
+				Quality    int    `json:"quality"`
+				Round      int    `json:"round"`
+				Single     string `json:"single"`
+				Tradable   string `json:"tradable"`
+			} `json:"earbuds"`
+			Hat struct {
+				Blanket   int    `json:"blanket"`
+				Craftable string `json:"craftable"`
+				Defindex  int    `json:"defindex"`
+				Plural    string `json:"plural"`
+				Price     struct {
+					Currency   string  `json:"currency"`
+					Difference float64 `json:"difference"`
+					Value      float64 `json:"value"`
+					ValueHigh  float64 `json:"value_high"`
+				} `json:"price"`
+				Priceindex int    `json:"priceindex"`
+				Quality    int    `json:"quality"`
+				Round      int    `json:"round"`
+				Single     string `json:"single"`
+				Tradable   string `json:"tradable"`
+			} `json:"hat"`
+			Keys struct {
+				Blanket   int    `json:"blanket"`
+				Craftable string `json:"craftable"`
+				Defindex  int    `json:"defindex"`
+				Plural    string `json:"plural"`
+				Price     struct {
+					Currency   string  `json:"currency"`
+					Difference float64 `json:"difference"`
+					Value      float64 `json:"value"`
+					ValueHigh  float64 `json:"value_high"`
+				} `json:"price"`
+				Priceindex int    `json:"priceindex"`
+				Quality    int    `json:"quality"`
+				Round      int    `json:"round"`
+				Single     string `json:"single"`
+				Tradable   string `json:"tradable"`
+			} `json:"keys"`
+			Metal struct {
+				Blanket   int    `json:"blanket"`
+				Craftable string `json:"craftable"`
+				Defindex  int    `json:"defindex"`
+				Plural    string `json:"plural"`
+				Price     struct {
+					Currency   string  `json:"currency"`
+					Difference float64 `json:"difference"`
+					Value      float64 `json:"value"`
+					ValueHigh  float64 `json:"value_high"`
+				} `json:"price"`
+				Priceindex int    `json:"priceindex"`
+				Quality    int    `json:"quality"`
+				Round      int    `json:"round"`
+				Single     string `json:"single"`
+				Tradable   string `json:"tradable"`
+			} `json:"metal"`
+		} `json:"currencies"`
+		Name    string `json:"name"`
+		Success int    `json:"success"`
+		URL     string `json:"url"`
+	} `json:"response"`
+}
+
+func getCurrencyData() Currency {
+	var c Currency
+	currencyData, _ := getWebJSON("http://backpack.tf/api/IGetCurrencies/v1/?key=56eaba18dea9e90b24c42c59")
+	if currencyData.statuscode != 200 {
+		panic(currencyData.statuscode)
+	}
+	err := json.Unmarshal([]byte(currencyData.body), &c)
+	if err != nil {
+		log.Fatal(err)
+		return c
+	}
+	return c
 }
